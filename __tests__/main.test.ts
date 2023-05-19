129bc081f20c4cc7efa6fb6df45912e733637892
Adam Chalmers <adam.s.chalmers@gmail.com>
2023-05-19 09:49:47 -0700

    Update __tests__/main.test.ts
    Co-authored-by: Alessio Perugini <alessioper.98@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -23,6 +23,9 @@ describe("filename tests", () => {
     ["protoc-3.20.2-linux-s390_64.zip", "linux", "s390x"],
     ["protoc-3.20.2-osx-aarch_64.zip", "darwin", "arm64"],
     ["protoc-3.20.2-osx-x86_64.zip", "darwin", "x64"]
+    ["protoc-3.20.2-osx-x86_64.zip", "darwin", "x64"],
+    ["protoc-3.20.2-win64.zip", "win32", "x64"],
+    ["protoc-3.20.2-win32.zip", "win32", "x32"],
   ];
   for (const [expected, plat, arch] of tests) {
     it(`downloads ${expected} correctly`, () => {
